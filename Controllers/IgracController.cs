b1aaf9cae277e2dc32041405fee65b3b12548474
nikolam00 <nikola.micovic00@gmail.com>
2022-01-01 02:02:05 +0100

    Dorada

--- Controllers/IgracController.cs
+++ Controllers/IgracController.cs
@@ -1,11 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using Models;
+
 
 namespace Web_Projekat_Sah.Controllers
 {
-    
+    [ApiController]
+    [Route("[controller]")]
+    public class IgracController : ControllerBase
+    {
+        public Context Context { get; set; }
+
+        public IgracController(Context context)
+        {
+            Context = context;
+        }
+        ///{Datum_rodjenja}
+        [Route("Unos igraca/{FideId}/{Ime}/{Prezime}/{Klub}")]
+        [HttpPost]
+        public async Task<ActionResult> Dodaj_igraca(int FideId,  string Ime, string Prezime, DateTime Datum_rodjenja, int Rating, Titula Title, Klub Klub)
+        {
+            if (Ime == "") return BadRequest("Morate uneti ime igraca");
+            if (Ime.Length > 20) return BadRequest("Pogresna duzina!");
+
+            if (Prezime == "") return BadRequest("Morate uneti ime igraca");
+            if (Prezime.Length > 20) return BadRequest("Pogresna duzina!");
+
+            if(FideId<0||FideId>999999) return BadRequest("Pogresan FideId!");
+
+            if (Datum_rodjenja.Year < 1940) return BadRequest("Pogrsan datum rodjenja!");
+
+            if (Rating < 1200 || Rating > 3000) return BadRequest("Pogresna vrednost za rejting!");
+
+            Igrac player = new Igrac();
+
+            player.Fide=FideId;
+            player.Ime = Ime;
+            player.Prezime = Prezime;
+            player.Datum_rodjenja = Datum_rodjenja;
+            player.Rejting = Rating;
+            player.Titula = Title;
+            player.Klub = Klub;
+
+            try
+            {
+                Context.Igraci.Add(player);
+                await Context.SaveChangesAsync();
+                return Ok("Igrac je dodat u bazu!");
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+        }
+
+
+        [Route("Brisanje igraca/{FideId}")]
+        [HttpDelete]
+        public async Task<ActionResult> Izbrisi_igraca(int FideId)
+        {
+            if(FideId<0 || FideId>999999) return BadRequest("Pogresna vrednost za FideId!");
+
+            try
+            {
+                var Igrac=Context.Igraci.Where(p=>p.Fide==FideId).FirstOrDefault();
+                if(Igrac!=null)
+                {
+                        Context.Igraci.Remove(Igrac);
+                        await Context.SaveChangesAsync();
+                        return Ok($"Igrac sa FideId brojem {FideId} je uspesno izbrisan!");
+                }
+                else
+                {
+                    return Ok("Takav igrac ne postoji!");
+                }
+
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
 }
---
+++ Controllers/KlubController.cs
@@ -0,0 +1,94 @@
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+using Models;
+
+
+namespace Web_Projekat_Sah.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class KlubController : ControllerBase
+    {
+        public Context Context { get; set; }
+
+        public KlubController(Context context)
+        {
+            Context = context;
+        }
+        ///{Datum_rodjenja}
+        [Route("Unos kluba/{Naziv}/{Mesto}/{Broj_Telefona}")]
+        [HttpPost]
+        public async Task<ActionResult> Dodaj_klub(string Naziv, string Mesto, string Broj_Telefona)
+        {
+            if (Naziv == "") return BadRequest("Morate uneti ime kluba");
+            if (Naziv.Length > 50) return BadRequest("Pogresna duzina naziv!");
+
+            var Klub = Context.Klubovi.Where(p=>p.Naziv.CompareTo(Naziv)==0).FirstOrDefault();
+
+            if(Klub!=null)
+            {
+                return BadRequest("Klub sa ovim imenom je vec osnovan!");
+            }
+
+            if (Mesto == "") return BadRequest("Morate uneti mesto");
+            if (Mesto.Length > 20) return BadRequest("Pogresna duzina mesto!");
+
+            if (Broj_Telefona == "") return BadRequest("Morate uneti broj telefona!");
+            if (Broj_Telefona.Length > 20) return BadRequest("Pogresna duzina broj telefona!");
+
+            Klub Club = new Klub();
+
+            Club.Naziv = Naziv;
+            Club.Mesto = Mesto;
+            Club.Broj_Telefona = Broj_Telefona;
+            Club.Broj_Igraca = 0;
+
+            try
+            {
+                Context.Klubovi.Add(Club);
+                await Context.SaveChangesAsync();
+                return Ok($"Klub {Naziv} je uspesno dodat u bazu!");
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.InnerException.Message);
+            }
+        }
+
+
+        [Route("Brisanje kluba/{Naziv}")]
+        [HttpDelete]
+        public async Task<ActionResult> Izbrisi_klub(string Naziv)
+        {
+             if (Naziv == "") return BadRequest("Morate uneti ime kluba");
+            if (Naziv.Length > 50) return BadRequest("Pogresna duzina naziv!");
+
+            try
+            {
+               var Klub = Context.Klubovi.Where(p=>p.Naziv.CompareTo(Naziv)==0).FirstOrDefault();
+
+                if(Klub!=null)
+                {
+                    Context.Klubovi.Remove(Klub);
+                    await Context.SaveChangesAsync();
+                    return Ok($"Klub {Naziv} je uspesno izbrisan!");
+                }
+                else
+                {
+                    return Ok("Takav klub ne postoji!");
+                }
+
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
