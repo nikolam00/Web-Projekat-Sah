5ce4f458d47aaba5f058758045052e4856f43b5a
nikolam00 <nikola.micovic00@gmail.com>
2022-01-01 04:22:16 +0100

    Dorada

--- Controllers/IgracController.cs
+++ Controllers/IgracController.cs
@@ -23,7 +23,7 @@ namespace Web_Projekat_Sah.Controllers
         ///{Datum_rodjenja}
         [Route("Unos igraca/{FideId}/{Ime}/{Prezime}/{Naziv_kluba}")]
         [HttpPost]
-        public async Task<ActionResult> Dodaj_igraca(int FideId,  string Ime, string Prezime, DateTime Datum_rodjenja, int Rating, Titula Title,string Naziv_kluba)
+        public async Task<ActionResult> Dodaj_igraca(int FideId, string Ime, string Prezime, DateTime Datum_rodjenja, int Rating, Titula Title, string Naziv_kluba)
         {
             if (Ime == "") return BadRequest("Morate uneti ime igraca");
             if (Ime.Length > 20) return BadRequest("Pogresna duzina!");
@@ -31,37 +31,37 @@ namespace Web_Projekat_Sah.Controllers
             if (Prezime == "") return BadRequest("Morate uneti ime igraca");
             if (Prezime.Length > 20) return BadRequest("Pogresna duzina!");
 
-            if(FideId<0||FideId>999999) return BadRequest("Pogresan FideId!");
+            if (FideId < 0 || FideId > 999999) return BadRequest("Pogresan FideId!");
 
             if (Datum_rodjenja.Year < 1940) return BadRequest("Pogrsan datum rodjenja!");
 
             if (Rating < 1200 || Rating > 3000) return BadRequest("Pogresna vrednost za rejting!");
 
-            if(Naziv_kluba=="") return BadRequest("Morate uneti ime Kluba");
+            if (Naziv_kluba == "") return BadRequest("Morate uneti ime Kluba");
 
             Igrac player = new Igrac();
 
-            player.Fide=FideId;
+            player.Fide = FideId;
             player.Ime = Ime;
             player.Prezime = Prezime;
             player.Datum_rodjenja = Datum_rodjenja;
             player.Rejting = Rating;
             player.Titula = Title;
 
-            var Klub = Context.Klubovi.Where(p=>p.Naziv.CompareTo(Naziv_kluba)==0).FirstOrDefault();
+            var Klub = Context.Klubovi.Where(p => p.Naziv.CompareTo(Naziv_kluba) == 0).FirstOrDefault();
 
-            if(Klub==null)
+            if (Klub == null)
             {
                 return BadRequest($"Uneti klub {Naziv_kluba} ne postoji!");
             }
 
-            player.Klub=Klub;
+            player.Klub = Klub;
 
             try
             {
                 Context.Igraci.Add(player);
                 await Context.SaveChangesAsync();
-                return Ok("Igrac je dodat u bazu!");
+                return Ok($"Igrac {Ime} {Prezime} je dodat u bazu!");
 
             }
             catch (Exception e)
@@ -75,16 +75,19 @@ namespace Web_Projekat_Sah.Controllers
         [HttpDelete]
         public async Task<ActionResult> Izbrisi_igraca(int FideId)
         {
-            if(FideId<0 || FideId>999999) return BadRequest("Pogresna vrednost za FideId!");
+            if (FideId < 0 || FideId > 999999) return BadRequest("Pogresna vrednost za FideId!");
 
             try
             {
-                var Igrac=Context.Igraci.Where(p=>p.Fide==FideId).FirstOrDefault();
-                if(Igrac!=null)
+                var Igrac = Context.Igraci.Where(p => p.Fide == FideId).FirstOrDefault();
+                if (Igrac != null)
                 {
-                        Context.Igraci.Remove(Igrac);
-                        await Context.SaveChangesAsync();
-                        return Ok($"Igrac sa FideId brojem {FideId} je uspesno izbrisan!");
+                    string Name = Igrac.Ime;
+                    string SurName = Igrac.Prezime;
+
+                    Context.Igraci.Remove(Igrac);
+                    await Context.SaveChangesAsync();
+                    return Ok($"Igrac sa FideId brojem {FideId}, {Name} {SurName} je uspesno izbrisan!");
                 }
                 else
                 {
@@ -92,20 +95,19 @@ namespace Web_Projekat_Sah.Controllers
                 }
 
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return BadRequest(e.Message);
             }
         }
 
-
-        [Route("Pregledaj/{FideId}")]
+        [Route("Pregledaj igraca/{FideId}")]
         [HttpGet]
         public ActionResult Vrati_igraca(int FideId)
         {
             if (FideId < 0 || FideId > 999999) return BadRequest("Pogresna vrednost za FideId!");
 
-            var Igrac = Context.Igraci.Where(p => p.Fide == FideId).Include(p=>p.Klub).FirstOrDefault();
+            var Igrac = Context.Igraci.Where(p => p.Fide == FideId).Include(p => p.Klub).FirstOrDefault();
 
             return Ok(Igrac);
         }
